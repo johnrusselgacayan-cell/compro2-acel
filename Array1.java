1318f64f0ce5e1ccedca10b198bdc47779ed279a
cell <johnrussel.gacayan@lorma.edu>
2026-01-13 13:44:29 +0800

    commitment

--- Array1.java
+++ Array1.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class Array1 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         int[] nums = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
 
         for(int i = 0; i < nums.length; i++){
             System.out.println(nums[i]);
+        }
+
+         System.out.print("Search: ");
+         int search = s.nextInt();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == search) {
+                System.out.println("index: " + i);
+                return;
+            }
         }
     }
 }
