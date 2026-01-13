618db4d44e23556d80c5cfd10b3195f3d8e837ab
cell <johnrussel.gacayan@lorma.edu>
2026-01-13 13:24:53 +0800

    Array1 10numbers

---
+++ Array1.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
